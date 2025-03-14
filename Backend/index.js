86c354500217344259053ea8ab9c8bf4f48c97c3
Parshva <codewithpasshu@gmail.com>
2025-03-14 11:21:25 +0530

    Error solved

--- Backend/index.js
+++ Backend/index.js
@@ -5,15 +5,15 @@ const app = express();
 
 // middle ware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions ={
-    origin:'http/localhost:5173',
-    credentials:true
-}
+const corsOptions = {
+  origin: "http//localhost:5173",
+  credentials: true,
+};
 app.use(cors(corsOptions));
 
 const PORT = 3000;
-app.listen(PORT,()=>{
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
